39cc8580849029f637d054568d1caf12c06858c8
fdwilliams <fdwilliams@users.noreply.github.com>
2022-02-19 23:22:35 -0500

    Add search bar and initial query functionality

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Lifeway Corinth Code Challenge
+# Star Wars Character Viewer
+*(Lifeway Corinth Code Challenge)*  
+
+## TODO
+* MUI
+* Star Wars theme
+* search autocomplete (with delay)
+  * auto select first in list; if no results, disable navigation
+* on search, append to history
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,57 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 import logo from './logo.svg';
 import './App.css';
 
+function SearchBar({query, setQuery, onSearch}) {
+  function onSubmit(e) {
+    onSearch();
+
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  function onChange(text) {
+    //TODO: delay
+    setQuery(text);
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={query}
+        placeholder="Luke Skywalker"
+        onInput={e => onChange(e.target.value)}
+      />
+      <button>Go!</button>
+    </form>
+  )
+}
+
 function App() {
+  var [query, setQuery] = useState("");
+
+  async function search(query) {
+    var data = (await axios.get(`https://swapi.dev/api/people/?search=${query}`)).data;
+    console.log(data);
+  }
+
+  function onSearch() {
+    if(query.trim() == "") {
+      //TODO: display message
+      return;
+    }
+
+    console.log(query);
+    search(query);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SearchBar query={query} setQuery={setQuery} onSearch={onSearch}/>
       </header>
     </div>
   );
